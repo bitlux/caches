679457f1ffe26f18c8f33531a853310ec6c23073
Adam <bitlux@users.noreply.github.com>
2018-06-02 23:14:38 -0700

    Create math.go

---
+++ math.go
@@ -0,0 +1,23 @@
+package math
+
+func divisors(n int) []int {
+	d := []int{1, n}
+	for i := 2; i <= n/2; i++ {
+		if n % i == 0 {
+			d = append(d, i)
+		}
+	}
+	return d
+}
+
+func digits(n int) []int {
+    d := []int{}
+    for n > 0 {
+        d = append(d, n%10)
+        n /= 10
+    }
+    for i := 0; i < len(d)/2; i++ {
+        d[i], d[len(d)-i-1] = d[len(d)-i-1], d[i]
+    }
+    return d
+}
