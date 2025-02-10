f95934512f911e306e3f8c315117362dbd4f938c
Adam Bender <abender@gmail.com>
2025-02-10 00:12:00 -0800

    Reformat

--- gc8jhr8_small_fuzzy_numbers/main.go
+++ gc8jhr8_small_fuzzy_numbers/main.go
@@ -6,16 +6,8 @@ import (
 	"github.com/d4l3k/go-bfloat16"
 )
 
-var floats = []float32{
-	7.112e-37,
-	5.031e-05,
-	5.049e-28,
-	3.964e-06,
-}
-
 func main() {
-	for _, f := range floats {
+	for _, f := range []float32{7.112e-37, 5.031e-05, 5.049e-28, 3.964e-06} {
 		fmt.Printf("%x\n", bfloat16.FromFloat32(f))
-
 	}
 }
