5972f713f03476293b7989f87f0749184e11a71d
Adam Bender <abender@gmail.com>
2025-05-09 22:20:45 -0700

    Add solution for GC6CNW6

--- gc6cnw6_dire_con_sequence_s_1/main.go
+++ gc6cnw6_dire_con_sequence_s_1/main.go
@@ -9,16 +9,11 @@ func v(n int) int {
 	if n%2 == 0 {
 		return v(n-1) + n - 1
 	}
-	return v(n-1) - (n-3)/2
-
-	// n /= 2
-	// return n*(n+1)/2 + 1
+	n /= 2
+	return n*(n+1)/2 + 1
 }
 
 func main() {
-	for i := range 20 {
-		fmt.Println(i, v(i))
-	}
 	for _, i := range []int{17, 41, 80, 1, 13, 28, 179} {
 		fmt.Println(i, v(i))
 	}
