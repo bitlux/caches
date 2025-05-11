363c173e6cdf28ef61235ba5f3055b4544653ec3
Adam Bender <abender@gmail.com>
2025-05-11 14:58:55 -0700

    Use closed form for GC6CNW6

--- gc6cnw6_dire_con_sequence_s_1/main.go
+++ gc6cnw6_dire_con_sequence_s_1/main.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-func v(n int) int {
-	if n == 0 {
-		return 0
-	}
+func negativeOneToTheN(n int) int {
 	if n%2 == 0 {
-		return v(n-1) + n - 1
+		return 1
 	}
-	n /= 2
-	return n*(n+1)/2 + 1
+	return -1
+}
+
+func v(n int) int {
+	return (n*n + 3*n*(negativeOneToTheN(n)+1) + 7*(negativeOneToTheN(n+1)+1)/2) / 8
 }
 
 func main() {
+	for i := range 20 {
+		fmt.Println(i, v(i))
+	}
+	fmt.Println()
 	for _, i := range []int{17, 41, 80, 1, 13, 28, 179} {
 		fmt.Println(i, v(i))
 	}
