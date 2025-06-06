4beec19650c9321487aab0e522a9ab9a232a7d5e
Adam Bender <abender@gmail.com>
2025-06-05 23:11:51 -0700

    Move candidate generation into the solver

--- gc4v7c9_alien_kenken/common/common.go
+++ gc4v7c9_alien_kenken/common/common.go
@@ -1,6 +1,11 @@
 package common
 
-import "math"
+import (
+	"maps"
+	"math"
+	"slices"
+	"strconv"
+)
 
 func round(n float64) int {
 	return int(math.Round(n - 0.5))
@@ -50,3 +55,54 @@ func Diamonds(nums []int) int {
 }
 
 const SIZE = 9
+
+func unique(nums [][]int) [][]int {
+	m := map[string]bool{}
+	for _, slice := range nums {
+		var s string
+		slices.Sort(slice)
+		for _, n := range slice {
+			s += strconv.Itoa(n)
+		}
+		m[s] = true
+	}
+
+	var ret [][]int
+	for _, k := range slices.Sorted(maps.Keys(m)) {
+		n, _ := strconv.Atoi(k)
+		ret = append(ret, digits(n))
+	}
+	return ret
+}
+
+func digits(n int) []int {
+	var d []int
+	for n > 0 {
+		d = append(d, n%10)
+		n /= 10
+	}
+	return d
+}
+
+func recurse(length int, soFar [][]int) [][]int {
+	if length == 0 {
+		return unique(soFar)
+	}
+	var ret [][]int
+	for n := 1; n < 10; n++ {
+		for _, curr := range soFar {
+			ret = append(ret, append(curr, n))
+		}
+	}
+	return recurse(length-1, ret)
+}
+
+func Candidates(f func([]int) int, target int, size int) [][]int {
+	var ret [][]int
+	for _, cand := range recurse(size, [][]int{{}}) {
+		if f(cand) == target {
+			ret = append(ret, cand)
+		}
+	}
+	return ret
+}
