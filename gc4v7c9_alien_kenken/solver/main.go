1e8623fa4d68d6b0c37b97e80d432443db412f7d
Adam Bender <abender@gmail.com>
2025-06-08 21:38:38 -0700

    Working solution for GC4V7C9

--- gc4v7c9_alien_kenken/solver/main.go
+++ gc4v7c9_alien_kenken/solver/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"maps"
 	"slices"
 	"strconv"
 	"time"
@@ -11,7 +10,7 @@ import (
 	"github.com/bitlux/caches/util"
 )
 
-const SIZE = common.SIZE
+const SIZE = 9
 
 var (
 	validCount, invalidCount int
@@ -25,6 +24,7 @@ type Cage struct {
 }
 
 var cages = []Cage{
+	// row 1
 	{
 		candidates: [][]int{{1, 5, 9}},
 		cells:      []string{"A1", "B1", "C1"},
@@ -38,6 +38,7 @@ var cages = []Cage{
 		cells:      []string{"G1", "H1", "I1"},
 	},
 
+	// row 2
 	{
 		candidates: [][]int{{7, 9}},
 		cells:      []string{"A2", "A3"},
@@ -59,6 +60,7 @@ var cages = []Cage{
 		cells:      []string{"I2", "I3"},
 	},
 
+	// row 3
 	{
 		candidates: common.Candidates(common.Hearts, 39, 4),
 		cells:      []string{"C3", "D3", "C4", "D4"},
@@ -68,6 +70,7 @@ var cages = []Cage{
 		cells:      []string{"F3", "G3", "F4", "G4"},
 	},
 
+	// row 4
 	{
 		candidates: [][]int{{1, 3}, {2, 3}},
 		cells:      []string{"A4", "A6"}, // 9h
@@ -81,6 +84,7 @@ var cages = []Cage{
 		cells:      []string{"I4", "I6"}, // 18h
 	},
 
+	// row 5
 	{
 		candidates: [][]int{{3, 5}},
 		cells:      []string{"B5", "C5"}, // 34c
@@ -94,8 +98,9 @@ var cages = []Cage{
 		cells:      []string{"G5", "H5"}, // 15s
 	},
 
+	// row 6
 	{
-		candidates: common.Candidates(common.Spades, 130, 5),
+		candidates: common.Candidates(common.Clubs, 130, 5),
 		cells:      []string{"B6", "B7", "B8", "C8", "D8"},
 	},
 	{
@@ -111,6 +116,7 @@ var cages = []Cage{
 		cells:      []string{"H6", "H7", "F8", "G8", "H8"},
 	},
 
+	// row 7
 	{
 		candidates: common.Candidates(common.Diamonds, 84, 5),
 		cells:      []string{"A7", "A8", "A9", "B9", "C9"},
@@ -124,14 +130,15 @@ var cages = []Cage{
 		cells:      []string{"I7", "I8", "G9", "H9", "I9"},
 	},
 
-	// {
-	// 	candidates: [][]int{{1, 9}, {4, 8}},
-	// 	cells:      []string{"D9", "F9"}, // 24s
-	// },
-	// {
-	// 	candidates: [][]int{{3}, {7}},
-	// 	cells:      []string{"E9"}, // 24s
-	// },
+	// row 9
+	{
+		candidates: [][]int{{1, 9}, {4, 8}},
+		cells:      []string{"D9", "F9"}, // 24s
+	},
+	{
+		candidates: [][]int{{3}, {7}},
+		cells:      []string{"E9"}, // 24s
+	},
 }
 
 func sheetsToIndices(s string) (int, int) {
@@ -140,16 +147,13 @@ func sheetsToIndices(s string) (int, int) {
 	return i - 1, util.A1Z26(int(s[0]) - 1)
 }
 
-type Board struct {
-	// Current board state
-	values [SIZE][SIZE]int
-}
+type Board [SIZE][SIZE]int
 
 func (b *Board) String() string {
 	s := ""
 	for i := range SIZE {
 		for j := range SIZE {
-			s += fmt.Sprintf("%d ", b.values[i][j])
+			s += fmt.Sprintf("%d ", b[i][j])
 		}
 		s += "\n"
 	}
@@ -160,7 +164,7 @@ func (b *Board) clone() *Board {
 	ret := &Board{}
 	for i := range SIZE {
 		for j := range SIZE {
-			ret.values[i][j] = b.values[i][j]
+			ret[i][j] = b[i][j]
 		}
 	}
 	return ret
@@ -169,12 +173,12 @@ func (b *Board) clone() *Board {
 func (b *Board) isValid() bool {
 	for row := range SIZE {
 		for colA := range SIZE {
-			a := b.values[row][colA]
+			a := b[row][colA]
 			if a == 0 {
 				continue
 			}
 			for colB := colA + 1; colB < SIZE; colB++ {
-				b := b.values[row][colB]
+				b := b[row][colB]
 				if b == 0 {
 					continue
 				}
@@ -188,12 +192,12 @@ func (b *Board) isValid() bool {
 
 	for col := range SIZE {
 		for rowA := range SIZE {
-			a := b.values[rowA][col]
+			a := b[rowA][col]
 			if a == 0 {
 				continue
 			}
 			for rowB := rowA + 1; rowB < SIZE; rowB++ {
-				b := b.values[rowB][col]
+				b := b[rowB][col]
 				if b == 0 {
 					continue
 				}
@@ -211,11 +215,10 @@ func (b *Board) isValid() bool {
 func (b *Board) recurse(index int) {
 	if farthest[index] == "" {
 		farthest[index] = b.String()
+		fmt.Printf("depth %d\n%s", index, b)
 	}
 	if index == len(cages) {
-		if validCount%1_000_000 == 0 {
-			fmt.Println(b)
-		}
+		fmt.Printf("Valid solution found!\n%s", b)
 		validCount++
 		return
 	}
@@ -227,7 +230,7 @@ func (b *Board) recurse(index int) {
 			clone := b.clone()
 			for pc, cell := range cage.cells {
 				i, j := sheetsToIndices(cell)
-				clone.values[i][j] = p[pc]
+				clone[i][j] = p[pc]
 			}
 			if clone.isValid() {
 				clone.recurse(index + 1)
@@ -261,22 +264,22 @@ func dump() {
 }
 
 func main() {
-	farthest = map[int]string{}
-
-	b := &Board{}
-	b.values[4][0] = 4
-	b.values[4][8] = 1
-
+	start := time.Now()
+	defer func() {
+		dump()
+		fmt.Println("execution time:", time.Since(start))
+	}()
 	go func() {
 		for range time.Tick(5 * time.Second) {
 			dump()
 		}
 	}()
 
-	b.recurse(0)
-	dump()
+	farthest = map[int]string{}
 
-	for _, d := range slices.Sorted(maps.Keys(farthest)) {
-		fmt.Printf("%d\n%s\n\n", d, farthest[d])
-	}
+	b := &Board{}
+	b[4][0] = 4
+	b[4][8] = 1
+
+	b.recurse(0)
 }
