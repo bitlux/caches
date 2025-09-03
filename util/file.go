4ecbcdfd0c6e88e302fb098aa561200c97801d3e
Adam Bender <abender@gmail.com>
2025-09-02 20:50:14 -0700

    Make ReadLines more robust. Make Digits more general

--- util/file.go
+++ util/file.go
@@ -5,21 +5,37 @@ import (
 	"os"
 )
 
-// ReadLines opens the named file and returns a slice of the lines of the
-// file.
-func ReadLines(file string) []string {
+const maxBuf = 1024 * 1024 * 1024 // 1 GiB
+
+func readLines(file string, size int) ([]string, error) {
 	f, err := os.Open(file)
 	Must(err)
 
-	scanner := bufio.NewScanner(f)
-	size := 100 * 1024
 	buf := make([]byte, size)
+	scanner := bufio.NewScanner(f)
 	scanner.Buffer(buf, size)
 
 	var ret []string
 	for scanner.Scan() {
 		ret = append(ret, scanner.Text())
 	}
-	Must(scanner.Err())
-	return ret
+	return ret, scanner.Err()
+}
+
+// ReadLines opens the named file and returns a slice of the lines of the file.
+func ReadLines(file string) []string {
+	size := bufio.MaxScanTokenSize
+	for size < maxBuf {
+		ret, err := readLines(file, size)
+		if err == nil {
+			return ret
+		}
+		if err == bufio.ErrTooLong {
+			size *= 2
+			continue
+		}
+		Must(err)
+	}
+	Must(bufio.ErrTooLong)
+	return nil
 }
